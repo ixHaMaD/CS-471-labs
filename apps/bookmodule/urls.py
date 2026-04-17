51ee79bf1a14cefc5abd03e90912bdb8fdd10c09
ixHaMaD <7xD.1422@gmail.com>
2026-04-17 22:13:34 +0300

    Lab6 finished, needs a revision

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('html5/text/formatting', views.text,name='books.text'),
     path('html5/listing', views.listing, name='books.listing'),
     path('html5/tables', views.tables, name="books.tables"),
+    path('search', views.search, name="books.search"),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse
 def index2(request, val1 = 0):
     return HttpResponse("value1 = "+str(val1))
 
-def viewbook(request, bookid):
+def viewbook2(request, bookid):
     #assume these book in a database
     book1 = {'id':123, 'title': 'Continuous Delivery','author':'J. Humble and D.Farley'}
     book2 = {'id':456, 'title': 'Secrets of Revers Engineering', 'author':'E. Eilam'}
@@ -52,3 +52,29 @@ def listing(request):
 
 def tables(request):
     return render(request, 'bookmodule/tables.html')
+
+def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        # now filter
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodule/bookList.html', {'books':newBooks})
+    return render(request, 'bookmodule/search.html')
+
+
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
+
